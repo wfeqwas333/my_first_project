7622f75b3a1618a36b1a7570f5d56c952439cf55
refwrgwre <rewgtdrwesdf@mail.com>
2021-10-10 13:32:11 -0500

    message

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template 
 
 app = Flask(__name__)
 
@@ -6,12 +6,19 @@ app = Flask(__name__)
 
 #Твой код
 
-@app.route('/')
-@app.route('/home')
+@app.route('/1')
+@app.route('/home1')
 def home():
     return render_template('index.html')
 
+@app.route('/')
+@app.route('/home')
+def download():
+	return render_template('download.html')
 
+@app.route('/wallpaper')
+def wallpaper():
+	return render_template('wallpaper.html')
 ####################################################
 
 if __name__ == "__main__":
